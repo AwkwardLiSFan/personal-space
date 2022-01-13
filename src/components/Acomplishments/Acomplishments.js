6183a567f674604e595c33e24ec2b720289522f2
AwkwardLiSFan <sohamaf30@gmail.com>
2022-01-14 00:06:36 +1030

    updated writing links

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -4,10 +4,10 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
+  { number: 'Jan \'22', text: 'The Rusting Birthday', link: "https://thisisnotagamereview.blogspot.com/2022/01/the-rusting-birthday.html"},
   { number: 'Jan \'22', text: 'Rohinton Mistry\'s Family Matters: An Emotional Tour-de-Force', link: "https://thisisnotagamereview.blogspot.com/2022/01/rohinton-mistrys-family-matters.html"},
   { number: 'Dec \'21', text: 'What Open-World Games Can Learn from Horizon: Zero Dawn\'s Collectibles', link: "https://www.gamerswaypoint.com/open-world-games-learn-from-horizon-zero-dawn-collectibles/"},
-  { number: 'Nov \'21', text: 'Witcher 3: War, Speciesism, and the Beauty of Long Form Storytelling', link: "https://www.gamerswaypoint.com/witcher-3-war-speciesism-beauty-of-long-form-storytelling/"},
-  { number: 'Nov \'21', text: 'P.T., Purgatory and Forgiveness in Visage: A Story Analysis', link: "https://www.gamerswaypoint.com/pt-purgatory-forgiveness-visage-story-analysis/"}
+  { number: 'Nov \'21', text: 'Witcher 3: War, Speciesism, and the Beauty of Long Form Storytelling', link: "https://www.gamerswaypoint.com/witcher-3-war-speciesism-beauty-of-long-form-storytelling/"}
 ];
 
 const Acomplishments = () => (
